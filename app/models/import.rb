460153aa03e3ea53d4121ab4c5f1180987e3e836
Chris McClymont <chris@mcclymont.it>
2018-07-27 21:30:54 +1000

    Import model: add after_commit run job, defaults and validation

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,6 +1,31 @@
+require 'zip'
+
 class Import < ApplicationRecord
   has_many :dishes
   has_many :menus
   has_many :menu_items
   has_many :menu_pages
+
+  after_initialize :set_defaults
+  after_commit -> { ImportZipJob.perform_later id }, on: :create, if: -> { status == 'uploaded' }
+
+  validates :zip_filepath, presence: true
+  validate :file_type_zip
+
+  private
+
+  def file_type_zip
+    return if zip_filepath.blank?
+
+    zip = Zip::File.open(zip_filepath)
+  rescue StandardError => e
+    Rails.logger.error e
+    errors.add :zip_data, 'is not a valid zip file'
+  ensure
+    zip&.close
+  end
+
+  def set_defaults
+    self.status ||= 'uploaded'
+  end
 end
