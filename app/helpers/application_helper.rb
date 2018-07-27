c065e596700f127044403954eb10e42c8c4868a9
Chris McClymont <chris@mcclymont.it>
2018-07-27 21:40:28 +1000

    Add nav_link and null_helper helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? ' active' : ''
+    content_tag(:li, class: 'nav-item' + class_name) do
+      link_to link_text, link_path, class_name: 'nav-link'
+    end
+  end
+
+  def null_helper(value)
+    value.present? ? value : '[BLANK]'
+  end
 end
